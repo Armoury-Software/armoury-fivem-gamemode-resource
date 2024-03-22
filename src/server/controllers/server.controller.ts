09e0eb8c2a7bf4799733b73a21bd68d798b8b4bd
Radu Mazilu <radumazilu24@gmail.com>
2024-03-22 18:36:52 +0200

    Initial implementation, draft 1

--- src/server/controllers/server.controller.ts
+++ src/server/controllers/server.controller.ts
@@ -1,6 +1,31 @@
-import { Controller } from '@armoury/fivem-framework';
+import { Controller, Export } from '@armoury/fivem-framework';
+import { type IItemBase } from '@armoury/fivem-roleplay-gamemode';
 
 @Controller()
 export class Server {
-    // TODO: Implement stuff here
+  private readonly _itemCache: IItemBase[] = [];
+
+  @Export()
+  public addToItemCache(...items: IItemBase[]): void {
+    items.forEach((item) => {
+      const existingCachedItem = this._itemCache.find((cachedItem) => cachedItem.identifier === item.identifier);
+      if (existingCachedItem) {
+        console.warn(
+          `[Armoury:] An item with identifier ${item.identifier} is planned to be registered, but an item with this identifier has already been cached. Be aware that this can cause issues.`
+        );
+        this._itemCache[this._itemCache.indexOf(existingCachedItem)] = item;
+      } else {
+        this._itemCache.push(item);
+      }
+
+      console.log(
+        `[Armoury:] Successfully registered item with identifier ${item.identifier} (WPU: ${item.weightPerUnit}, FOS: ${item.fullyOccupiesSlot}).`
+      );
+    });
+  }
+
+  @Export()
+  public getCachedItems(): IItemBase[] {
+    return this._itemCache;
+  }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,4 @@
-import { Server_Init } from '@armoury/fivem-framework';
+import { Server_Init } from '@armoury/fivem-roleplay-gamemode';
 import { Server } from './controllers/server.controller';
 
 Server_Init(Server);
